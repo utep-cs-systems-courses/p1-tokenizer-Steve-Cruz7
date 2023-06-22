78aaeb3c1125754afa1d69438f57635a50f82662
Steve Cruz <sacruz7@miners.utep.edu>
2023-06-22 13:57:49 -0600

    Updated tokens.c with all methods, not tested

--- src/tokens.c
+++ src/tokens.c
@@ -24,3 +24,38 @@ char *token_start(char *s){
   else
      return s;
 }
+
+char *token_terminator(char *token){
+  while(*token != '\0')
+    token++;
+  return token;
+}
+
+int count_tokens(char *str){
+  int count = 0;
+  str = token_start(str);
+  while(*str != '\0'){
+    if(not_space_char(*str)){
+	str++;
+   }else{
+        count++;
+        str = token_start(str);
+    }
+  }
+  return count;
+}
+
+char *copy_str(char *inStr, int len){
+  char *p;
+  while(*s != '\0'){
+    *p = *s;
+    s++;
+    p++;
+  }
+  *p = '\0';
+  p -= len;
+  return p;
+}
+      
+      
+ 
